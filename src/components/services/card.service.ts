c10c58e41c3303ba3736d7f68e93fb781a8b5389
Alexandr <golobokov2604@gmail.com>
2023-04-29 15:13:40 +0200

    Changed database

--- src/components/services/card.service.ts
+++ src/components/services/card.service.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
 import { ICardProps } from "@/interfaces/ICard";
 
-const API_URL = 'https://641bf9349b82ded29d5cdf68.mockapi.io';
+const API_URL = "https://643af31070ea0e66028bd3f4.mockapi.io";
 axios.defaults.baseURL = API_URL;
 
 export const CardService = {
-    async getAll() {
-        const { data } = await axios.get<ICardProps[]>('/cards');
-        return data;
-    },
+  async getAll() {
+    const { data } = await axios.get<ICardProps[]>("/cards");
+    return data;
+  },
 
-    async getById(id: string) {
-        const { data } = await axios.get<ICardProps[]>('/cards', {
-            params: {
-                id
-            }
-        });
-        return data[0];
-    }
+  async getById(id: string) {
+    const { data } = await axios.get<ICardProps[]>("/cards", {
+      params: {
+        id,
+      },
+    });
+    return data[0];
+  },
 };
