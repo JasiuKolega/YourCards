c390c68696c595a81d10181a1fb12d19c58ded58
Alexandr <golobokov2604@gmail.com>
2023-04-29 15:06:37 +0200

    Changed database

--- src/components/services/section.service.ts
+++ src/components/services/section.service.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { ISection } from "@/interfaces/ISection";
 
-const API_URL = 'https://641bf9349b82ded29d5cdf68.mockapi.io';
+const API_URL = "https://643af31070ea0e66028bd3f4.mockapi.io";
 axios.defaults.baseURL = API_URL;
 
 export const SectionService = {
-    async getAll() {
-        const { data } = await axios.get<ISection[]>('/sections');
-        return data;
-    }
+  async getAll() {
+    const { data } = await axios.get<ISection[]>("/cards");
+    return data;
+  },
 };
